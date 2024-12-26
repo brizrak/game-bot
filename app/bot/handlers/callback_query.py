b665b63f771e4739254af3ca22c3a745ac261133
Benismann <91243913+Benismannn@users.noreply.github.com>
2024-12-26 16:46:54 +0700

    ...
    Дурашка 3

--- app/bot/handlers/callback_query.py
+++ app/bot/handlers/callback_query.py
@@ -5,6 +5,7 @@ from aiogram.fsm.context import FSMContext
 
 from app.bot.handlers.delete_message import delete_previous_message
 from app.bot.handlers.blackjack import play_start
+from app.bot.handlers.fool import play_start_fool
 
 router = Router()
 router.callback_query.filter(F.message.chat.type == "private", StateFilter(None))
@@ -17,7 +18,7 @@ async def handler(call: CallbackQuery, state: FSMContext) -> None:
     if call.data == "sea":
         await call.message.answer("Здесь будет морской бой")
     if call.data == "fool":
-        await call.message.answer("Здесь наверное ниче не будет")
+        await play_start_fool(call.message, state)
     await delete_previous_message(state, call.message)
 
     await call.answer()
