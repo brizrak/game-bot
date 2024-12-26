68b4a88858fbed1ae43636d82d2144e057b6dfc7
Benismann <91243913+Benismannn@users.noreply.github.com>
2024-12-26 16:57:39 +0700

    Вроде конец дурашки
    дурачелло

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Config(BaseSettings):
-    # model_config = SettingsConfigDict(env_file='.env', extra='allow')
+    model_config = SettingsConfigDict(env_file='.env', extra='allow')
 
     BOT_TOKEN: str
 
@@ -13,13 +13,8 @@ class Config(BaseSettings):
     PACK_PATH: str
     RESULT_PATH: str
 
-    # def dsn(self) -> str:
-    #     return f"redis://{self.REDIS_HOST}:{self.REDIS_PORT}/{self.REDIS_DB}"
+    def dsn(self) -> str:
+        return f"redis://{self.REDIS_HOST}:{self.REDIS_PORT}/{self.REDIS_DB}"
 
 
-config = Config(BOT_TOKEN="6579477792:AAFzRL7T-noWx3Ce74mR7qgfE-xCBuzaQYM",
-                REDIS_HOST="redis",
-                REDIS_PORT = 6379,
-                REDIS_DB = 0,
-                PACK_PATH = "C:\\Users\\Admin\\PycharmProjects\\game-bot\\app\\колода52",
-                RESULT_PATH = "C:\\Users\\Admin\\PycharmProjects\\game-bot\\app\\users_result")
+config = Config()
